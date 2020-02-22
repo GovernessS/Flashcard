a334eea735321e07df2bc8f773819071cb2006d2
GovernessS <simps422@umn.edu>
2020-02-21 22:15:34 -0600

    added optional features to app

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -2,23 +2,66 @@ package com.example.flashcard;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isShowingIcon = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView) findViewById(R.id.barack)).setVisibility(View.INVISIBLE);
+        findViewById(R.id.toggle_choices_invisibility).setVisibility(View.INVISIBLE);
 
-        findViewById(R.id.q).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.Bill).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((TextView) findViewById(R.id.barack)).setVisibility(View.VISIBLE);
-                ((TextView) findViewById(R.id.q)).setVisibility(View.INVISIBLE);
+                findViewById(R.id.Bill).setBackgroundColor(getResources().getColor(R.color.incorrectAns, null));
+                findViewById(R.id.barack).setBackgroundColor(getResources().getColor(R.color.correctAnswer, null));
+            }
+        });
+
+        findViewById(R.id.George).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.George).setBackgroundColor(getResources().getColor(R.color.incorrectAns, null));
+                findViewById(R.id.barack).setBackgroundColor(getResources().getColor(R.color.correctAnswer, null));
+            }
+        });
+
+        findViewById(R.id.barack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.barack).setBackgroundColor(getResources().getColor(R.color.correctAnswer, null));
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.Bill).setBackgroundColor(getResources().getColor(R.color.normal, null));
+                findViewById(R.id.George).setBackgroundColor(getResources().getColor(R.color.normal, null));
+                findViewById(R.id.barack).setBackgroundColor(getResources().getColor(R.color.normal, null));
+            }
+        });
+
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isShowingIcon) {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_iconmonstr_eye_5);
+                    findViewById(R.id.Bill).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.George).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.barack).setVisibility(View.INVISIBLE);
+                    isShowingIcon = false;
+                } else {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_iconmonstr_eye_8);
+                    findViewById(R.id.Bill).setVisibility(View.VISIBLE);
+                    findViewById(R.id.George).setVisibility(View.VISIBLE);
+                    findViewById(R.id.barack).setVisibility(View.VISIBLE);
+                    isShowingIcon = true;
+                }
             }
         });
     }
